3aae9d85f41fd175b0cc229df0c3e890f2282b8d
Ioannis Sarantopoulos <ioannis.sarantopoulos@factset.com>
2020-05-08 21:29:47 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './styles/style.scss';
+let isLightTheme = true;
+document.querySelector("body > header > div.right > label > span").addEventListener('click', () => {
+    isLightTheme = !isLightTheme;
 
-console.log("Hello webpack");
-console.log("Hello webpack 2");
+    localStorage.setItem('social-media-dashboard-theme', (isLightTheme ? 'light' : 'dark'));
+
+    setTheme();
+});
+
+function setTheme() {
+    document.querySelector('body').classList.remove((isLightTheme ? 'dark' : 'light'));
+    document.querySelector('body').classList.add((isLightTheme ? 'light' : 'dark'));
+}
+
+if (localStorage.getItem('social-media-dashboard-theme') && localStorage.getItem('social-media-dashboard-theme') !== '') {
+    isLightTheme = localStorage.getItem('social-media-dashboard-theme') === 'light';
+}
+
+setTheme();
